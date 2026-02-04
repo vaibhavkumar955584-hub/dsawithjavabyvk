e3d804ed7b3f40fe740438d4fa0fa013e80dee10
vaibhav kumar <vaibhavkumar955584@gmail.com>
2026-02-04 12:06:21 +0530

    a magical number file add

---
+++ src/magicnumber.java
@@ -0,0 +1,14 @@
+public class magicnumber {
+    public static void main(String[] args) {
+        int n=7;
+        int ans=0;
+        int base=5;
+        while(n>0){
+            int last=n&1;
+            n=n>>1;
+            ans+=last*base;
+            base*=5;
+        }
+        System.out.println(ans);
+    }
+}
