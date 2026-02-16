78dc9b9738fe416425b6127516f6caff1cc75c39
vaibhav kumar <vaibhavkumar955584@gmail.com>
2026-02-16 23:39:47 +0530

    Inheritance and polymorphism

---
+++ Oops/Polymorphism/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Shapes shape = new Shapes();
+        Shapes circle = new Circle();
+        Shapes square = new Square();
+
+        circle.area();
+    }
+}
